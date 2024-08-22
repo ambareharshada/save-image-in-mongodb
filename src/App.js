02fb3a16d724692d024a0a5e09f269f7be5e79ea
ambareharshada <ambareharshada88@gmail.com>
2024-08-22 11:47:01 +0530

    changes done

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import { IoMdCloudUpload } from "react-icons/io";
 import "./App.css";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 function App() {
   const [displayImage, setDisplayImage] = useState("");
-  const imageBase64 = (file) => {
+  const [displayAllImage, setDisplayAllImage] = useState([]);
+
+  const imageBase64 = async (file) => {
     const reader = new FileReader();
-    reader.readAsDataURL(file);
+    await reader.readAsDataURL(file);
     const data = new Promise((resolve, reject) => {
       reader.onload = () => resolve(reader.result);
       reader.onerror = (err) => reject(err);
@@ -20,26 +22,66 @@ function App() {
     // console.log(image)
     setDisplayImage(image);
   };
+
+  const fetchImage = async () => {
+    const res = await fetch("http://localhost:5003/");
+    const data = await res.json();
+    setDisplayAllImage(data.data);
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    if (displayImage) {
+      const res = await fetch("http://localhost:5003/upload", {
+        method: "POST",
+        headers: {
+          "content-type": "application/json",
+        },
+        body: JSON.stringify({ image: displayImage }),
+      });
+      const data = await res.json();
+      console.log(data);
+      if (data.success) {
+        alert(data.message);
+        setDisplayImage("");
+        fetchImage();
+      }
+    }
+  };
+
+  useEffect(()=>{
+    fetchImage()
+  },[])
   return (
     <>
       <div className="imageContainer">
         <form>
           <label htmlFor="uploadImage">
             <div className="uploadBox">
-            <input
+              <input
                 type="file"
                 id="uploadImage"
                 onChange={handleUploadImage}
               />
-              {displayImage ? <img src={displayImage} alt="Images" /> : <IoMdCloudUpload />}
-
-             
+              {displayImage ? (
+                <img src={displayImage} alt="Images" />
+              ) : (
+                <IoMdCloudUpload />
+              )}
             </div>
           </label>
           <div className="btn">
-            <button>UPLOAD</button>
+            <button onClick={handleSubmit}>UPLOAD</button>
           </div>{" "}
         </form>
+
+        <div className="all-image">
+          {displayAllImage.map((obj) => {
+            return (
+              <img src={obj.image} alt="" width={"320px"} height={"300px"} />
+            );
+          })}
+        </div>
       </div>
     </>
   );
